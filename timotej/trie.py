00b30eff7479909337fc0381aaa3d3c5b392aff0
Timotej Toepfer <toepfert@ag202-12.arcig>
2020-02-27 16:28:15 +0100

    translated tria.py in english

--- timotej/trie.py
+++ timotej/trie.py
@@ -2,8 +2,9 @@ class node:
   def __init__(self, character):
     self.character = character
     self.lower_nodes = []
+    self.counter = Counter()
     
-  def create(self, character):
+  def create_node(self, character):
     # ~ print("tvořím nový uzel \""+pismeno+"\"")
     next_node = node(character)
     self.lower_nodes.append(next_node)
@@ -17,74 +18,57 @@ class node:
         return(lower)
     return(False)
     
-  def ulozse(self):
-    odpoved=self.pismeno
-    for poduzel in self.poduzly:
-      odpoved+=poduzel.ulozse()
-    odpoved+=")"
-    return(odpoved)
+  def save(self):
+    result = self.character
+    for lower in self.lower_nodes:
+      result += lower.save()
+    result += ")"
+    return(result)
     
-class koncovyuzel:
-  def __init__(self):
-    self.pocet=1
-    self.pismeno=">"
-  def ulozse(self):
-    odpoved=self.pismeno+str(self.pocet)+")"
-    return(odpoved)
-        
-    
-def pridejslovo(slovo):
-  zkuzel=zacatek                #zkoumaný uzel
-  while len(slovo)>0:
+  def add_word(word):
+    if len(word) > 0:
+      wanted_character = word[0]
+      word = word[1:]
+      if counter[wanted_character] > 0:
+        next_node = self.find_next(wanted_character)
+      else:
+        next_node = self.create_node(wanted_character)   
+      next_node.add_word(word)
+    else:
+      next_node = self.create_node(" ")
+    self.counter[wanted_character] += 1
+
+# ~ def find(starting_node):
+  # ~ word=input("zadejte slovo: ")
+  # ~ word+=">"
+  # ~ actual_node=starting node                #zkoumaný uzel
+  # ~ while len(slovo)>0:
     # ~ print(slovo)
-    zkpismeno=slovo[0]
-    slovo=slovo[1:]
-    dalsi=zkuzel.zjistidalsi(zkpismeno)
-    if dalsi==False:
-      dalsi=zkuzel.vytvordalsi(zkpismeno)      
-    zkuzel=dalsi
-  dalsi=zkuzel.zjistidalsi(">")
-  if dalsi==False:
-    dalsi=zkuzel.vytvordalsi(">")
-  else:
-    dalsi.pocet+=1
+    # ~ zkpismeno=slovo[0]
+    # ~ slovo=slovo[1:]
+    # ~ dalsi=zkuzel.zjistidalsi(zkpismeno)
+    # ~ if dalsi==False:
+      # ~ return("Slovo ve slovníku není")
+    # ~ zkuzel=dalsi
+  # ~ pocet=zkuzel.pocet
+  # ~ return("Slovo ve slovníku je "+str(pocet)+"krát")
+def add_to_tria(starting_node, dataset):
+  full_text = open(dataset, "r")
+  lines = full_text.readlines()
+  word = ""
+  for line in lines:
+    for character in line:
+      if character != " ":
+        word += character
+      elif len(word) > 0:
+        starting_node.add_word(word)
+        word = ""
+  full_text.close()
+  return(starting_node)
 
-def najdi(zacatek):
-  slovo=input("zadejte slovo: ")
-  slovo+=">"
-  zkuzel=zacatek                #zkoumaný uzel
-  while len(slovo)>0:
-    print(slovo)
-    zkpismeno=slovo[0]
-    slovo=slovo[1:]
-    dalsi=zkuzel.zjistidalsi(zkpismeno)
-    if dalsi==False:
-      return("Slovo ve slovníku není")
-    zkuzel=dalsi
-  pocet=zkuzel.pocet
-  return("Slovo ve slovníku je "+str(pocet)+"krát")
-
-zacatek=uzel("<")
-
-soubor=open("text.txt","r")
-radky=soubor.readlines()
-slovo=""
-for radek in radky:
-  radek=radek.lower()
-  for znak in radek:
-    if znak in (list("aábcčdďeéěfghiíjklmnňoópqrřsštťuúůvwxyýzž")):
-      slovo+=znak
-    elif len(slovo)>0:
-      pridejslovo(slovo)
-      slovo=""
-soubor.close()
-
-kulozeni=zacatek.ulozse()
-ulozene=open("trie.txt","w")
-ulozene.write(kulozeni)
-ulozene.close()
-
-while True:
-  print(najdi(zacatek))
-
+def export_tria(starting_node):
+  to_save = starting_node.save()
+  export_file = open("tria.txt","w")
+  export_file.write(to_save)
+  export_file.close()
 
