34e51616578be52f7bf25c8b0a2923d7ac3d0f1c
NotVeryGoodPythonUser <58443103+NotVeryGoodPythonUser@users.noreply.github.com>
2020-03-16 22:03:27 +0100

    finished tria

---
+++ timotej/tria.py
@@ -0,0 +1,106 @@
+from collections import Counter
+
+class node:
+  def __init__(self, upper_node, character):
+    self.upper_node = upper_node
+    self.character = character
+    self.lower_nodes = []
+    self.likelihood = 0
+    
+  def create_node(self, character):
+    next_node = node(self, character)
+    self.lower_nodes.append(next_node)
+    return(next_node)
+    
+  def find_next(self, wanted_character):
+    if len(self.lower_nodes)  == 0:
+      return(False)
+    for lower in self.lower_nodes:
+      if lower.character == wanted_character:
+        return(lower)
+    return(False)
+    
+  def save(self):
+    result = self.character
+    result += str(self.likelihood)
+    for lower in self.lower_nodes:
+      result += lower.save()
+    result += ")"
+    return(result)
+    
+  def add_word(self, word):
+    wanted_character = word[0]
+    word = word[1:]
+    next_node = self.find_next(wanted_character)
+    if next_node == False:
+      next_node = self.create_node(wanted_character)
+    next_node.likelihood += 1
+    if len(word) > 0:
+      next_node.add_word(word)
+
+  def predict(self, word):
+    print(word)
+    if len(word) == 0:
+      print("getting best option")
+      best = "None"
+      best_likelihood =0
+      for node in self.lower_nodes:
+        print (node.character, node.likelihood)
+        if node.likelihood > best_likelihood:
+          best = node.character
+          best_likelihood = node.likelihood
+      return(best)
+    else:
+      print("searching for next node")
+      wanted_character = word[0]
+      word = word[1:]
+      next_node = self.find_next(wanted_character)
+      if next_node == False:
+        return("No idea :-(")
+      else:
+        return(next_node.predict(word))
+
+def add_to_tria(starting_node, dataset):
+  full_text = open(dataset, "r", encoding = "utf-8")
+  lines = full_text.read()
+  word = ""
+  for character in lines:
+    if character in list("AÁBCČDĎEÉFGHIÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽaábcčdďeéfghiíjklmnňoópqrřsštťuůúvwxyýzž,.><"):
+      word += character
+    elif len(word) > 0:
+      starting_node.add_word(word+">")
+      word = ""
+  full_text.close()
+  return(starting_node)
+
+def export_tria(starting_node):
+  to_save = starting_node.save()
+  export_file = open("tria.txt", "w", encoding = "utf-8")
+  export_file.write(to_save)
+  export_file.close()
+
+def load_tria(file_name):
+  exported = open(file_name, "r", encoding = "utf-8")
+  string = exported.read()
+  starting_node = node(None, "<")
+  actual_node = starting_node
+  actual_node.likelihood = ""
+  for character in string:
+    if character in list("AÁBCČDĎEÉFGHIÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽaábcčdďeéfghiíjklmnňoópqrřsštťuůúvwxyýzž,.><"):
+      actual_node = actual_node.create_node(character)
+      actual_node.likelihood = ""
+    elif character in list("1234567890"):
+      actual_node.likelihood += character
+    elif character == ")":
+      actual_node.likelihood = int(actual_node.likelihood)
+      actual_node = actual_node.upper_node
+      
+  return(starting_node)
+
+starting_node = node(None, "<")
+add_to_tria(starting_node,"ustava.txt")
+export_tria(starting_node)
+while True:
+  task = input("napište část slova ")
+  next_letter = starting_node.predict(task)
+  print(task, next_letter)
